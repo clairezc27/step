833b3e9c2a4b5ca052a4d24a6e17c3ab13996156
Claire <clairezc27@gmail.com>
2020-06-15 15:37:07 +0000

    pass optionsForNoAttendees(), noOptionsForTOoLongOfARequest(), eventSplitsRestriction()

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,45 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+
+    if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+      return Arrays.asList();
+    }
+
+    if (events.isEmpty()) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+    ArrayList<TimeRange> timeList = new ArrayList<>();
+    Collection<TimeRange> toReturn = new ArrayList<TimeRange>();
+    int start = TimeRange.START_OF_DAY;
+    boolean endDay = true;
+    int endTime = 0;
+    for (Event e : events) {
+      int eventStart = e.getWhen().start();
+      int eventEnd = eventStart + e.getWhen().duration();
+      if (eventStart == start) {
+        start = eventStart + e.getWhen().duration(); 
+      }
+
+      TimeRange toAdd = TimeRange.fromStartEnd(start, eventStart, false);
+      toReturn.add(toAdd);
+      start = eventEnd;
+      
+      if (eventStart + e.getWhen().duration() < TimeRange.END_OF_DAY) {
+        endDay = false;
+        endTime = eventEnd;
+      }
+    }
+
+    if (!endDay) {
+      toReturn.add(TimeRange.fromStartEnd(endTime, TimeRange.END_OF_DAY, true));
+    }
+
+    return toReturn;
   }
 }
